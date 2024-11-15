924353944cd4b94f946918041be6524fcb14762f
jdaly3411 <jdaly3411@gmail.com>
2024-11-15 11:41:41 +0000

    Added listpaging functionality

--- public/react/components/PagesList.js
+++ public/react/components/PagesList.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import { Page } from './Page'
+import React from "react";
+import { Page } from "./Page";
 
-export const PagesList = ({ pages }) => {
-  return <>
-		{
-			pages.map((page, idx) => {
-				return <Page page={page} key={idx} />
-			})
-		}
-	</>
-}
+export const PagesList = ({ pages, setCurrentPage }) => {
+  return (
+    <div>
+      <h1>All pages</h1>
+      <ul>
+        {pages.map((page) => (
+          <li key={page.id}>
+            <button
+              onClick={async () => {
+                const response = await fetch(`/api/wiki/${page.slug}`);
+                const data = await response.json();
+                setCurrentPage(data);
+              }}
+            >
+              {page.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PagesList;
