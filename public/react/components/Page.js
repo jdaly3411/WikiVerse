98e1b06be5bede20875c87df3bbda09b2f5e3f15
jdaly3411 <jdaly3411@gmail.com>
2024-11-15 11:45:05 +0000

    Added page functionality

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export const Page = (props) => {
-  return <>
-    <h3>{props.page.title}</h3>
-  </>
-}
+export const Page = ({ page, setCurrentPage }) => {
+  <div>
+    <h2>{page.title}</h2>
+    <p>
+      <strong>Author:</strong> {page.author.name}
+    </p>
+    <p>
+      <strong>Email</strong> {page.author.email}
+    </p>
+    <p>
+      <strong>Content:</strong>
+    </p>
+    <p>{page.content}</p>
+    <p>
+      <strong>Tags:</strong> {page.tags.join(", ")}
+    </p>
+    <button onClick={() => setCurrentPage(null)}>Back to Pages List</button>
+  </div>;
+};
